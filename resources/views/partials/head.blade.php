229ea94e0c241af92f67082aca5eb067a9c54709
Michael Stokoe <mstokoe0990@gmail.com>
2025-11-14 15:09:07 +0000

    Fixing dark mode styling

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -6,13 +6,65 @@
     .dark\:text-gray-400:where(.dark, .dark *) {
         color: rgb(156 163 175) !important;
     }
+    .dark\:text-gray-200:where(.dark, .dark *) {
+        color: rgb(229 231 235) !important;
+    }
+    .dark\:text-gray-100:where(.dark, .dark *) {
+        color: rgb(243 244 246) !important;
+    }
     .dark\:text-white:where(.dark, .dark *) {
         color: rgb(255 255 255) !important;
+    }
+    .dark\:text-purple-400:where(.dark, .dark *) {
+        color: rgb(192 132 252) !important;
+    }
+    .dark\:text-yellow-400:where(.dark, .dark *) {
+        color: rgb(250 204 21) !important;
+    }
+    .dark\:text-green-200:where(.dark, .dark *) {
+        color: rgb(187 247 208) !important;
+    }
+    .dark\:text-blue-400:where(.dark, .dark *) {
+        color: rgb(96 165 250) !important;
     }
     .dark\:bg-gray-800:where(.dark, .dark *) {
         background-color: rgb(31 41 55) !important;
     }
     .dark\:bg-gray-700:where(.dark, .dark *) {
         background-color: rgb(55 65 81) !important;
+    }
+    .dark\:bg-purple-900:where(.dark, .dark *) {
+        background-color: rgb(88 28 135) !important;
+    }
+    .dark\:bg-green-900:where(.dark, .dark *) {
+        background-color: rgb(20 83 45) !important;
+    }
+    .dark\:bg-blue-900:where(.dark, .dark *) {
+        background-color: rgb(30 58 138) !important;
+    }
+    .dark\:bg-yellow-900:where(.dark, .dark *) {
+        background-color: rgb(113 63 18) !important;
+    }
+    .dark\:hover\:bg-gray-600:hover:where(.dark, .dark *) {
+        background-color: rgb(75 85 99) !important;
+    }
+    .dark\:hover\:bg-gray-700:hover:where(.dark, .dark *) {
+        background-color: rgb(55 65 81) !important;
+    }
+    .dark\:hover\:text-blue-300:hover:where(.dark, .dark *) {
+        color: rgb(147 197 253) !important;
+    }
+    .dark\:hover\:text-red-300:hover:where(.dark, .dark *) {
+        color: rgb(252 165 165) !important;
+    }
+
+    .py-0\.5 {
+        padding-top: 0.125rem /* 2px */;
+        padding-bottom: 0.125rem /* 2px */;
+    }
+
+    .px-2\.5 {
+        padding-left: 0.625rem /* 10px */;
+        padding-right: 0.625rem /* 10px */;
     }
 </style>
